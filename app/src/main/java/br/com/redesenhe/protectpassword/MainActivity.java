adf007df9a72b075b665155a92a42e34a93e4021
Mayke Alisson <maykealison@gmail.com>
2020-07-19 18:27:56 -0300

    feat: criando ui home

--- app/src/main/java/br/com/redesenhe/protectpassword/MainActivity.java
+++ app/src/main/java/br/com/redesenhe/protectpassword/MainActivity.java
@@ -1,8 +1,11 @@
 package br.com.redesenhe.protectpassword;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.activty_main_toolbar);
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+//        Toolbar toolbar = findViewById(R.id.activty_main_toolbar);
+//        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
