374acc685c401a3e31a17880f479d97485d348d5
Mayke Alisson <maykealison@gmail.com>
2020-07-20 05:31:50 -0300

    feat: criando menus page home

--- app/src/main/java/br/com/redesenhe/protectpassword/MainActivity.java
+++ app/src/main/java/br/com/redesenhe/protectpassword/MainActivity.java
@@ -1,10 +1,13 @@
 package br.com.redesenhe.protectpassword;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,13 +22,27 @@ public class MainActivity extends AppCompatActivity {
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-//        Toolbar toolbar = findViewById(R.id.activty_main_toolbar);
-//        setSupportActionBar(toolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_home_op1:
+                Toast.makeText(MainActivity.this, "Menu 1", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.menu_home_op2:
+                Toast.makeText(MainActivity.this, "Menu 2", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
